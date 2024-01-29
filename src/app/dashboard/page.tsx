1b6dce9fe32a551ec49da4059816380cecbd20ab
Cesar Roman <cesardroman18@gmail.com>
2024-01-29 17:04:59 -0600

    added shadui

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Dashboard() {
+  return <button className="rounded-full">Save Changes</button>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Hello, world</div>;
+  return <button className="rounded-full">Hello, world</button>;
 }
