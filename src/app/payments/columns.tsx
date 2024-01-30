5bc6288d41e2d5125899eee909849dd47bc4a63e
Cesar Roman <cesardroman18@gmail.com>
2024-01-30 17:15:52 -0600

    added dropdownmenu component

--- src/app/payments/columns.tsx
+++ src/app/payments/columns.tsx
@@ -1,6 +1,18 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/DropdownMenu";
 
 export type Payment = {
   id: string;
